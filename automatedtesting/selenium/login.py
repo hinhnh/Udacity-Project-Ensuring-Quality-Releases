98098434f3c56925930c4cae94b06e95ece59000
hinhnh <hinh.nguyen@myporsperity.com.au>
2022-12-17 18:33:58 +0700

    update login.py

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -4,40 +4,39 @@ from selenium.webdriver.chrome.options import Options as ChromeOptions
 import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning) 
 import logging
-import syslog;
-# logging.basicConfig(
-#     filename='seleniumtestrun.log',
-#     filemode='a',
-#     format='%(asctime)s %(levelname)-8s %(message)s',
-#     level= syslog.syslog,
-#     datefmt='%Y-%m-%d %H:%M:%S')
+logging.basicConfig(
+    filename='seleniumtestrun.log',
+    filemode='a',
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S')
 
 options = ChromeOptions()
 options.add_argument("--headless") 
 driver = webdriver.Chrome(options=options)
 # print ('INFO: Browser started successfully. Navigating to the demo page to login.')
-syslog.syslog('Browser started successfully. Navigating to the demo page to login.')
+logging.info('Browser started successfully. Navigating to the demo page to login.')
 driver.get('https://www.saucedemo.com/')
 
 # Start the browser and login with standard_user
 def login (user, password):
     # print ('INFO: Starting the browser...')
-    syslog.syslog('Starting the browser...')
+    logging.info('Starting the browser...')
     driver.find_element_by_css_selector("input[id='user-name']").send_keys(user)
     driver.find_element_by_css_selector("input[id='password']").send_keys(password)
     driver.find_element_by_css_selector("input[id='login-button']").click()
     # print('INFO: Successfully logged in as ' + user )
-    syslog.syslog('Successfully logged in as ' + user)
+    logging.info('Successfully logged in as ' + user)
 
 def add_to_cart():
     # print('INFO: Adding all 6 items to cart')
-    syslog.syslog('Adding all 6 items to cart')
+    logging.info('Adding all 6 items to cart')
     items = driver.find_elements_by_css_selector("button.btn_primary.btn_inventory")
 
     for item in items:
         product = item.get_property("name")
         # print('INFO: ' + product + ' added to the cart')
-        syslog.syslog(product + ' added to the cart')
+        logging.info(product + ' added to the cart')
         item.click()
     cart_label = driver.find_element_by_css_selector('.shopping_cart_badge').text
     assert cart_label == '6'
@@ -46,13 +45,13 @@ def add_to_cart():
 def remove_from_cart():
     driver.find_element_by_css_selector("a[class='shopping_cart_link']").click()
     # print('INFO: Removing all 6 items to cart')
-    syslog.syslog('Removing all 6 items to cart')
+    logging.info('Removing all 6 items to cart')
     items = driver.find_elements_by_css_selector("button.cart_button")
 
     for item in items:
         product = item.get_property("name")
         # print('INFO: '+ product +' removed from the cart')
-        syslog.syslog(product +' removed from the cart')
+        logging.info(product +' removed from the cart')
         item.click()
     
 
